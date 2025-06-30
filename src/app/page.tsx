aaaf3c9177099663a1bea36c0e1af8c656a4d20f
abhilash01820 <mekalaabhilash01820@gmail.com>
2025-07-01 00:30:46 +0530

    upated the contact and about should be appear in the main page instead of going to the about file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import About from "./about/page";
 import Blogs from "./components/Blogs";
 import Hero from "./components/Hero";
 import Newsletter from "./components/Newsletter";
 import Projects from "./components/Projects";
+import Contact from "./contact/page";
 
 
 export default function Home() {
@@ -9,9 +11,10 @@ export default function Home() {
     <>
       <Hero /> 
       <Projects />
+      <About/>
       <Blogs />
       <Newsletter />
-      
+      <Contact/>
     </>
   );
 }
